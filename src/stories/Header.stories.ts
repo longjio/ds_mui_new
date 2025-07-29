1756e8fab4e756242284e4f312bcbe2585c7ab2f
230551 <longjiyoung@gmail.com>
2025-07-29 12:06:01 +0900

    storybook 업로드

---
+++ src/stories/Header.stories.ts
@@ -0,0 +1,36 @@
+// D:/ds_mui_new/src/stories/Header.stories.ts
+
+import type { Meta, StoryObj } from '@storybook/react';
+// ★★★ 'storybook/test'를 '@storybook/test'로 변경합니다. ★★★
+import { fn } from '@storybook/test';
+
+import { Header } from './Header';
+
+const meta = {
+  title: 'Example/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  args: {
+    onLogin: fn(),
+    onLogout: fn(),
+    onCreateAccount: fn(),
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {};
